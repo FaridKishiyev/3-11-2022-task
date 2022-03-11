1dce1d7f9d5d31ce875c0f3116c797c9c0fa58de
FaridKishiyev <99797424+FaridKishiyev@users.noreply.github.com>
2022-03-11 08:01:00 +0400

    Add files via upload

---
+++ taskbilmemkac/taskbilmem kac/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Class_and__Inheritance.Models;
+
+namespace Class_and__Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.productName = "Nusretin et kitabi";
+            book.authorName = "CZN burak";
+            book.pageCount = 331;
+            book.discountPercent = 12;
+            book.costPrice = 31;
+            book.salePrice = 68;
+            book.saledPrice = 37;
+            book.getInfo();
+            Console.WriteLine($"Endirim Faizi:   {book.GetDiscountedPrice()}");
+        }
+    }
+}
